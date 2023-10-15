2499cb53293c4bca47ce7629d876206f329c4083
Ian Clyde Tejada <ianclyde.tejada-16@cpu.edu.ph>
2023-10-15 14:47:10 +0800

    changes to 2.3

---
+++ lab2.3/database.ts
@@ -0,0 +1,33 @@
+import { Client } from "pg";
+
+async function createDatabaseClient() {
+  return new Client({
+    host: "localhost",
+    user: "postgres",
+    port: 5432,
+    password: "iwillbecomerich",
+    database: "postgres",
+  });
+}
+
+export async function connectDatabase() {
+  const client = createDatabaseClient();
+
+  try {
+    await (await client).connect();
+    console.log("Connected to the database");
+    return client;
+  } catch (error) {
+    console.error(`Error connecting to the database: ${error}`);
+    throw error;
+  }
+}
+
+export async function closeDatabase(client: Client) {
+  try {
+    await client.end();
+    console.log("Closed the database connection");
+  } catch (error) {
+    console.error(`Error closing the database connection: ${error}`);
+  }
+}
--- lab2.3/lending.ts
+++ lab2.3/lending.ts
@@ -1,50 +1,82 @@
 import * as http from "node:http";
-import { IncomingMessage, ServerResponse } from "node:http";
 import * as fs from "node:fs/promises";
-import { Pool } from "pg";
+import * as querystring from "querystring";
+import { connectDatabase, closeDatabase } from "./database";
+
+const server = http.createServer(handleRequest);
+const port = 3000;
 
 async function handleRequest(
-  request: IncomingMessage,
-  response: ServerResponse
+  request: http.IncomingMessage,
+  response: http.ServerResponse
 ) {
   const url = request.url;
   const method = request.method;
 
-  console.log("Debugging -- url is", url, "while method is", method);
-
   if (url === "/apply-loan") {
     try {
-      const contents = await fs.readFile(
-        "./lending-form.html"
-      ); /* how to read file contents again? */
+      const contents = await fs.readFile("./lending-form.html");
       response
-        .writeHead(200, { "Content-Type": "text/html" }) // tell the browser that you're sending HTML
+        .writeHead(200, { "Content-Type": "text/html" })
         .end(contents.toString());
     } catch (error) {
+      console.error(`Error serving the form: ${error}`);
       response.writeHead(500, { "Content-Type": "text/plain" });
-      response.end("Internal Server Error");
+      response.end("Internal Server Error at apply-loan");
     }
-  } else if (url === "/apply-loan-success") {
-    
+  } else if (url === "/apply-loan-success" && method === "POST") {
     try {
-      let body = ''
+      let body = "";
 
-      request.on('data', (chunk) => {
+      request.on("data", (chunk) => {
         body += chunk.toString();
-      })
-    }
+      });
 
+      request.on("end", async () => {
+        try {
+          const loanFormData = querystring.parse(body.toString());
+          const { name, email, phone_number, reason, amount } = loanFormData;
+
+          const insertQuery = `
+            INSERT INTO loans (name, email, phone_number, loan_reason, loan_amount)
+            VALUES ($1, $2, $3, $4, $5)
+          `;
+
+          const values = [name, email, phone_number, reason, Number(amount)];
+
+          const client = connectDatabase();
+
+          try {
+            await (await client).query(insertQuery, values);
+
+            response.writeHead(200, { "Content-Type": "text/plain" });
+            response.end("Loan application submitted successfully");
+          } catch (error) {
+            console.error(`Error inserting data into the database: ${error}`);
+            response.writeHead(500, { "Content-Type": "text/plain" });
+            response.end("Internal Server Error at query");
+          } finally {
+            closeDatabase(await client);
+          }
+        } catch (error) {
+          console.error(
+            `Error processing the form data or inserting into the database: ${error}`
+          );
+          response.writeHead(500, { "Content-Type": "text/plain" });
+          response.end("Internal Server Error 67");
+        }
+      });
+    } catch (error) {
+      console.error(`Error: ${error}`);
+      response.writeHead(500, { "Content-Type": "text/plain" });
+      response.end("Internal Server Error 73");
+    }
   } else {
-    response
-      // 200 tells the browser the response is successful, memorize the common ones: 200, 401, 403, 404, 500
-      // see https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-      .writeHead(200)
-      .end("You sent me:" + url);
+    response.writeHead(404, { "Content-Type": "text/plain" });
+    response.end("Not Found hi");
   }
 }
 
-const server = http.createServer(handleRequest);
-
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log("Server started at http://localhost:3000");
 });
